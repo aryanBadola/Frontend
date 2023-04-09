8700a265711594d63ea4725d12e348d92da65204
Aryan Badola <aryanbadola431@gmail.com>
2023-04-09 14:32:20 +0530

    fe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+
+import LoginSignUp from './components/LoginSignUp';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SlotBooking from './pages/SlotBooking';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      
+      <Routes>
+        <Route path='/' element = {<Home />} />
+        <Route path='/login' element = {<LoginSignUp />} />
+        <Route path='/slotbooking' element={<SlotBooking />} />
+      </Routes>
+    </BrowserRouter>
+
+     
+    </>
   );
 }
 
